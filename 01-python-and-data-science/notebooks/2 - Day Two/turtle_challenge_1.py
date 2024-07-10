7ec6e10166496832279389b44b7508933a854a1f
MrB333 <bellamy.michael5@gmail.com>
2024-07-10 09:45:04 -0600

    new update

--- 01-python-and-data-science/notebooks/2 - Day Two/turtle_challenge_1.py
+++ 01-python-and-data-science/notebooks/2 - Day Two/turtle_challenge_1.py
@@ -8,6 +8,66 @@ t = turtle.Turtle()
 # Challenge 1: Squares in a Grid
 
 # YOUR CODE HERE
+t.pencolor("green")
+t.speed(20)
+t.pendown()
+for i in range(0,4):
+    t.right(90)
+    t.forward(30)
+t.penup()
+t.forward(45)
+t.pendown()
+for i in range(0,4):
+    t.right(90)
+    t.forward(30)
+t.penup()
+t.forward(45)
+t.pendown()
+for i in range(0,4):
+    t.right(90)
+    t.forward(30)
+t.penup()
+t.left(90)
+t.forward(45)
+t.left(90)
+t.forward(30)
+t.pendown()
+for i in range(0,4):
+    t.right(90)
+    t.forward(30)
+t.penup()
+t.forward(45)
+t.pendown()
+for i in range(0,4):
+    t.right(90)
+    t.forward(30)
+t.penup()
+t.forward(45)
+t.pendown()
+for i in range(0,4):
+    t.right(90)
+    t.forward(30)
+t.penup()
+t.right(90)
+t.forward(90)
+t.right(90)
+t.forward(30)
+t.pendown()
+for i in range(0,4):
+    t.right(90)
+    t.forward(30)
+t.penup()
+t.forward(45)
+t.pendown()
+for i in range(0,4):
+    t.right(90)
+    t.forward(30)
+t.penup()
+t.forward(45)
+t.pendown()
+for i in range(0,4):
+    t.right(90)
+    t.forward(30)
 
 
 # DON'T TOUCH THIS
--- 01-python-and-data-science/notebooks/2 - Day Two/turtle_challenge_2.py
+++ 01-python-and-data-science/notebooks/2 - Day Two/turtle_challenge_2.py
@@ -4,7 +4,65 @@ window = turtle.Screen()
 window.setup(width=800, height=800)
 
 t = turtle.Turtle()
+t.color("white")
+t.penup
+t.setposition(-210, 210)
+t.color("brown")
+t.pendown
+t.circle(60, extent=360)
 
+t.color("white")
+t.penup
+t.forward(210)
+t.color("brown")
+t.pendown
+t.circle(60, extent=360)
+t.color("white")
+t.penup
+t.forward(210)
+t.color("brown")
+t.pendown
+t.circle(60, extent=360)
+t.color("white")
+t.penup
+t.setposition(-210, 0)
+t.color("brown")
+t.pendown
+t.circle(60, extent=360)
+
+t.color("white")
+t.penup
+t.forward(210)
+t.color("brown")
+t.pendown
+t.circle(60, extent=360)
+t.color("white")
+t.penup
+t.forward(210)
+t.color("brown")
+t.pendown
+t.circle(60, extent=360)
+t.color("white")
+t.penup
+t.setposition(-210, -210)
+t.color("white")
+t.pendown
+t.circle(60, extent=360)
+
+t.color("white")
+t.penup
+t.forward(210)
+t.color("brown")
+t.pendown
+t.circle(60, extent=360)
+t.color("white")
+t.penup
+t.forward(210)
+t.color("brown")
+t.pendown
+t.circle(60, extent=360)
+t.color("white")
+t.penup
 # Challenge 2: Circles in a Grid with One Missing
 
 # YOUR CODE HERE
